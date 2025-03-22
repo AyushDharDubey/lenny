609dc756f2eeb7405b4f54908103d3f940251ef4
Ayush Dhar Dubey <mailaddubey@gmail.com>
2025-03-22 19:55:17 +0530

    replace deprecated on_start with lifespan

--- lenny/app.py
+++ lenny/app.py
@@ -1,14 +1,19 @@
 import uvicorn
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from lenny.routes.api import router
 from lenny.models import init_db
 from lenny.configs import OPTIONS
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    init_db()
+    yield
+
+
 app = FastAPI(title="Lenny")
 
-@app.on_event("startup")
-def on_startup():
-    init_db()
 
 app.include_router(router, prefix="/v1/api")
 
