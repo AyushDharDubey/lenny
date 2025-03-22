4e3a52975e6fe09f1dd656a29cea9044c666a53a
Ayush Dhar Dubey <mailaddubey@gmail.com>
2025-03-22 14:48:20 +0530

    package models module

--- lenny/models/__init__.py
+++ lenny/models/__init__.py
@@ -1,5 +1,8 @@
 from sqlmodel import SQLModel, create_engine, Session
 from lenny.configs import DB_URI, DEBUG
+from .book import Book
+from .lending_record import LendingRecord
+from .waitlist import Waitlist
 
 engine = create_engine(DB_URI, echo=DEBUG, client_encoding='utf8')
 
@@ -11,3 +14,10 @@ def init_db():
 def get_session():
     with Session(engine) as session:
         yield session
+
+
+__all__ = [
+    "Book",
+    "LendingRecord",
+    "Waitlist"
+]
