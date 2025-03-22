eec2ca0730a38d013bd6fcd848db9f623d9c4765
Ayush Dhar Dubey <mailaddubey@gmail.com>
2025-03-22 19:56:03 +0530

    auto handle borrow_date and due_date

--- lenny/routes/lending_record.py
+++ lenny/routes/lending_record.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
 from sqlmodel import Session
+from datetime import timedelta, datetime
 from lenny.models import LendingRecord
 from lenny.schemas import LendingRecordCreate, LendingRecordRead
 from lenny.models import get_session
@@ -9,7 +10,10 @@ router = APIRouter(prefix="/lending", tags=["Lending Records"])
 
 @router.post("/", response_model=LendingRecordRead)
 def borrow_book(lending: LendingRecordCreate, session: Session = Depends(get_session)):
-    new_lending = LendingRecord(**lending.model_dump())
+    lending_data = lending.model_dump()
+    lending_data["borrow_date"] = datetime.now()
+    lending_data["due_date"] = lending_data["borrow_date"] + timedelta(days=14)
+    new_lending = LendingRecord(**lending_data)
     session.add(new_lending)
     session.commit()
     session.refresh(new_lending)
